fa013402e978c4659e9ff31842a46c94d17c3cf3
karinsjoo <karinsjoo@hotmail.com>
2025-12-22 21:49:35 +0100

    Bytt plats

--- CvAppenVS/Program.cs
+++ CvAppenVS/Program.cs
@@ -47,8 +47,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapStaticAssets();
 
