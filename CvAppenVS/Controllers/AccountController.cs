d57cf1c75c90e8c84da394f7d85aba00d9eb7fe4
fredrikthegel <fredrik.thegel@gmail.com>
2025-12-23 00:25:48 +0100

    login
    enkel loginsida

---
+++ CvAppenVS/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using CvAppenVS.Models;
+
+namespace CvAppenVS.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<User> userManager;
+        private SignInManager<User> signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+
+        public IActionResult LogIn()
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            return View(loginViewModel);
+        }
+    }
+    
+       
+}
---
+++ CvAppenVS/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CvAppenVS.Models
+
+
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Ange användarnamn.")]
+        [StringLength(255)]
+
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Ange lösenord")]
+        [DataType(DataType.Password)]
+
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+    
+}
