cca1d88f373a59ab2e06422966ba92b3029b60e5
fredrikthegel <fredrik.thegel@gmail.com>
2025-12-30 06:34:07 +0100

    registrera
    vy för att skapa konto

---
+++ CvAppenVS/Models/RegistreraViewmodel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CvAppenVS.Models
+{
+    public class RegistreraViewmodel
+    {
+        [Required(ErrorMessage = "Ange användarnamn.")]
+        [StringLength(30, ErrorMessage = "Användarnamnet får max vara 30 tecken.")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Skriv in lösenord.")]
+        [DataType(DataType.Password)]
+        
+
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Bekräfta lösenord.")]
+        [DataType (DataType.Password)]
+        [Display(Name  = "Bekräfta lösenord")]
+        [Compare("Password", ErrorMessage = "Lösenorden matchar inte.")]
+
+        public string BekraftaLosenord { get; set; }
+    }
+}
