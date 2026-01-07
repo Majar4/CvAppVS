99b5bb6f63194490314ae6031d904bba8cf8c977
AleLuh1 <83966239+AleLuh1@users.noreply.github.com>
2026-01-07 14:31:35 +0100

    Created ChangePasswordViewModel

---
+++ CvAppenVS/ViewModels/ChangePasswordViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CvAppen.Web.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set;  }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare("NewPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
