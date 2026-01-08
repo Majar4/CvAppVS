1733baff1b0ddd0d3a77cdad7cfb7e7dffcc83ea
AleLuh1 <83966239+AleLuh1@users.noreply.github.com>
2026-01-08 01:00:20 +0100

    Changed field titles from Eng to Swe for Account settings

--- CvAppenVS/ViewModels/AccountSettingsViewModel.cs
+++ CvAppenVS/ViewModels/AccountSettingsViewModel.cs
@@ -6,9 +6,14 @@ namespace CvAppen.Web.ViewModels
     {
         [Required(ErrorMessage = "Vänligen ange ett fullständigt namn")]
         [StringLength(50, MinimumLength = 2, ErrorMessage="Namnet måste vara mellan 2 och 50 tecken")]
+        [Display(Name = "Namn")]
         public string Name { get; set; }
+
         [StringLength(100, ErrorMessage="Adressen får inte vara över 100 tecken")]
+        [Display(Name = "Adress")]
         public string Address { get; set; }
+
+        [Display(Name = "Privat profil")]
         public bool IsPrivate { get; set;  }
     }
 }
